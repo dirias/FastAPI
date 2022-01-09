55340beb2683caefaa15f5fb38806e9082707780
Didier Irias Méndez <didier.irias@gmail.com>
2022-01-08 18:15:50 -0600

    validation_body

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 #Models
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -56,3 +62,19 @@ def show_person(
         )
     ):
     return {person_id: "created"}
+
+#Validaciones: Request Body 
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    Location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(Location.dict())
+    return results
